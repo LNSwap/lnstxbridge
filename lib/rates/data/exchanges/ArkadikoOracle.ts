b2a302c25060958fa22b6b3a34430cc576d565f3
pseudozach <git@pseudozach.com>
2021-11-08 23:06:02 -0800

    usda rate is OK

--- lib/rates/data/exchanges/ArkadikoOracle.ts
+++ lib/rates/data/exchanges/ArkadikoOracle.ts
@@ -3,15 +3,16 @@ import Exchange, { makeRequest } from '../Exchange';
 // import { Configuration, SmartContractsApi, TransactionsApi } from '@stacks/blockchain-api-client';
 
 import {
-  bufferCVFromString,
   callReadOnlyFunction,
+  stringAsciiCV,
+  cvToJSON,
 } from '@stacks/transactions';
 import { StacksMainnet } from '@stacks/network';
 
 const contractAddress = 'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR';
 const contractName = 'arkadiko-oracle-v1-1';
 const functionName = 'get-price';
-const buffer = bufferCVFromString('USDA');
+const buffer = stringAsciiCV('USDA');
 const network = new StacksMainnet();
 const senderAddress = 'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR';
 
@@ -30,23 +31,33 @@ class ArkadikoOracle implements Exchange {
   private static readonly API = 'https://api.coingecko.com/api/v3';
 
   public async getPrice(baseAsset: string, quoteAsset: string): Promise<number> {
+    if(baseAsset !== "BTC" || quoteAsset !== "USDA") {
+      throw "error";
+    }
     // BTC USDA
     console.log("ArkadikoOracle.10 getPrice baseAsset quoteAsset: ", baseAsset, quoteAsset);
     let longerquoteasset = this.longerName(quoteAsset);
+    let longerbaseasset = this.longerName(baseAsset);
     // let lowerbaseasset = baseAsset.toLowerCase();
-    let lowerbaseasset = this.longerName(baseAsset);
-    const pair = `${this.longerName(quoteAsset)}&vs_currencies=${baseAsset}`;
+    // let lowerbaseasset = this.longerName(baseAsset);
+    const pair = `${longerbaseasset}&vs_currencies=${longerquoteasset}`;
+    console.log(`Arkadiko.42 pair `, `${ArkadikoOracle.API}/simple/price?ids=${pair}`)
     const response = await makeRequest(`${ArkadikoOracle.API}/simple/price?ids=${pair}`);
-    console.log("response: ", response, response[longerquoteasset]);
-    const lastprice = response[longerquoteasset][lowerbaseasset];
-    console.log("ArkadikoOracle.18 1/lastprice: ", 1/lastprice);
+    console.log("response: ", response, response[longerbaseasset]);
+    const lastprice = response[longerbaseasset][longerquoteasset];
+    // 68318
+    console.log("ArkadikoOracle.18 btc lastprice: ", lastprice);
 
-    const result = await callReadOnlyFunction(options);
-    console.log(`ArkadikoOracle.45 getPrice: `, result)
+    const result:any = await callReadOnlyFunction(options);
+    console.log(`ArkadikoOracle.45 getPrice: `, result, cvToJSON(result));
+
+    let usdaperusd = Number(result.data['last-price'].value)/Number(result.data['decimals'].value)
+    let btcperusda = lastprice / usdaperusd;
+    console.log(`ArkadikoOracle.45 getPrice: `, result, usdaperusd, btcperusda)
     
     // 1 stx = 3000 sats
     // 1 btc = 33156.498673740054 STX -> this is returned from here which is correct on frontend UI
-    return Number(1/lastprice);
+    return Number(btcperusda);
 
     // const lastTrade = (Object.values(response['result'])[0] as Record<string, string[]>)['c'];
 
