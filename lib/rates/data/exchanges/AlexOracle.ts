2d20059a48adbe858b31b7747aa638e8f92c54fa
pseudozach <zozsoy@gmail.com>
2022-05-16 19:47:04 +0000

    cleanup logging

--- lib/rates/data/exchanges/AlexOracle.ts
+++ lib/rates/data/exchanges/AlexOracle.ts
@@ -42,7 +42,7 @@ class AlexOracle implements Exchange {
     // console.log('AlexOracle.58 options: ', options, options.functionArgs);
     try {
       const result:any = await callReadOnlyFunction(options);
-      console.log('AlexOracle.62 returning ', baseAsset, quoteAsset, cvToJSON(result).value.value);
+      // console.log('AlexOracle.62 returning ', baseAsset, quoteAsset, cvToJSON(result).value.value);
       return parseInt(cvToJSON(result).value.value)/10**8;
     } catch(error) {
       throw 'Alex Oracle failure';
